b9b2e80496dfb644914587648513d1bb3cd40d4a
Abdirahman <dubowhamaan@gmail.com>
2021-12-29 21:46:13 +0300

    final commit

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -7,14 +7,21 @@ import { Quote} from '../quote';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  @Input() quoty: Quote;
   @Output() isRead = new EventEmitter<boolean>();
   deleteQuote(read:boolean){
     this.isRead.emit(read)
   }
+  upvote(){
+    this.quoty.likes+=1;
+  }
+  downvote(){
+    this.quoty.dislikes+=1;
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit()  {
   }
 
 }
