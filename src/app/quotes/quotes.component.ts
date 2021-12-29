1eb042f07c13fe1571a4da75af815ba5c47c0b40
Abdirahman <dubowhamaan@gmail.com>
2021-12-29 12:43:16 +0300

    quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,6 +10,9 @@ export class QuotesComponent implements OnInit {
   title = 'Insightful Quotes';
   quotes:Quote[] = [
     new Quote (1,'Selb', 'Love Theory', 'Love all,trust a few,do wrong to none.', 'Tim Kim', new Date(2021,12,28),0,0),
+    new Quote (2,'Omar', 'Music', 'Music is like a dream.One that i cannot hear.', 'Ludwig Van Beethoven', new Date(2021,12,28),0,0),
+    new Quote (3,'Tom', 'Technology', 'Any sufficiantlyadvanced technology is equivalent to magic.', 'Arthur C. Clarke', new Date(2021,12,28),0,0),
+    new Quote (4,'Mohamed', 'Religion', 'Religion is the bridge between human races.', 'clay Shirky', new Date(2021,12,28),0,0),
   ]
   constructor() { }
 
