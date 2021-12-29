f5f3e2fdeca473599088f2943fe369399c9cdfdf
Abdirahman <dubowhamaan@gmail.com>
2021-12-29 13:51:56 +0300

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    HighlightQuoteDirective
   ],
   imports: [
     BrowserModule,
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -13,7 +13,12 @@ export class QuotesComponent implements OnInit {
     new Quote (2,'Omar', 'Music', 'Music is like a dream.One that i cannot hear.', 'Ludwig Van Beethoven', new Date(2021,12,28),0,0),
     new Quote (3,'Tom', 'Technology', 'Any sufficiantlyadvanced technology is equivalent to magic.', 'Arthur C. Clarke', new Date(2021,12,28),0,0),
     new Quote (4,'Mohamed', 'Religion', 'Religion is the bridge between human races.', 'clay Shirky', new Date(2021,12,28),0,0),
-  ]
+  ];
+  get sortQuotes() {
+    return this.quotes.sort((a,b) => {
+      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+    });
+  }
   constructor() { }
 
   ngOnInit(): void {
