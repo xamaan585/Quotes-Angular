3c0b4225aff5d114b3b2b65eae8286021db5d598
Abdirahman <dubowhamaan@gmail.com>
2021-12-29 11:39:10 +0300

    initial commit

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
+
 export class Quote {
+    showInfo: boolean;
+    constructor(public id:number,public name:string,public title:string,public quote:string,public author:string,public datePosted: Date,public likes:number,public dislike:number){
+        this.showInfo=false
+    }
 }
