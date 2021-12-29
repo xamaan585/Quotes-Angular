42a11ff7dc40030c2395c5054c8926ced7517499
Abdirahman <dubowhamaan@gmail.com>
2021-12-29 14:30:14 +0300

    latest input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightQuoteDirective } from './highlight-quote.directive';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DatePipePipe } from './date-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    HighlightQuoteDirective
+    HighlightQuoteDirective,
+    QuoteDetailsComponent,
+    DatePipePipe
   ],
   imports: [
     BrowserModule,
