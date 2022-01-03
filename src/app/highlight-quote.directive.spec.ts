66a10352b2e2298ef52daf7e15f826ada2161563
Abdirahman <dubowhamaan@gmail.com>
2022-01-03 10:56:29 +0300

    Auto-generated commit [ci skip]

--- src/app/highlight-quote.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightQuoteDirective } from './highlight-quote.directive';
-
-describe('HighlightQuoteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightQuoteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
